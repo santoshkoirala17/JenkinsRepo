418201232ecc509102f13ff21b99065435ef42f3
Santosh Koirala <santoshkoirala17@gmail.com>
2022-12-02 17:49:31 -0600

    This is jenkinsRepository oroject.

---
+++ src/main/java/HibernateSetup/JenkinsRepo/JenkinsClass.java
@@ -0,0 +1,11 @@
+package HibernateSetup.JenkinsRepo;
+
+public class JenkinsClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is jebkins testing class is generated.");
+
+	}
+
+}
