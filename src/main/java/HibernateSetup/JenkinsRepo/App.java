d9ee72a0ce5428ffeba1777b19e166d93e2661c5
santoshkoirala17 <santoshkoirala17@gmail.com>
2022-12-02 17:53:18 -0600

    Updated code

--- src/main/java/HibernateSetup/JenkinsRepo/App.java
+++ src/main/java/HibernateSetup/JenkinsRepo/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.ouy.println("This is updated code in the github.");
     }
 }
